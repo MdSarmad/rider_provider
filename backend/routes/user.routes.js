bdd2312aa6047e623afa32fc39089bc48b55cfc4
MdSarmad <28mdsarmad99@gmail.com>
2025-08-05 23:59:09 +0530

    login route added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,7 +7,13 @@ router.post("/register", [
     body("email").isEmail().withMessage("Invalid Email"),
     body("fullname.firstname").isLength({min: 3}).withMessage("First Name should be at least 3 characters long"),
     body("password").isLength({min: 6}).withMessage("Password must be at least 6 characters long")
-], userController.registerUser)
+], userController.registerUser);
+
+router.post("/login", [
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('password').isLength({ min: 6}).withMessage('Password must be at least 6 characters long')
+], userController.loginUser)
+
 
 
 module.exports = router;
