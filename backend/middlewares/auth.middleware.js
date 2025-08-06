030b6d5b6a21dcd8701011087b3e947eac01dc18
MdSarmad <28mdsarmad99@gmail.com>
2025-08-06 23:45:05 +0530

    cookies added

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -4,20 +4,20 @@ const jwt = require("jsonwebtoken");
 
 module.exports.authUser = async (req, res, next) => {
     const token = req?.cookies?.token || req?.headers?.authorization?.split?.(" ")?.[1] || "";
-    console.log({token})
-    if(!token) {
-        return res.status(401).json({message: "Unauthorized"})
+    console.log({ token })
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" })
     }
 
-    try { 
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await userModel.findById(decoded?._id);
 
         req.user = user;
 
         return next();
-    } catch(error) {
-        console.log({error})
-        return res.status(401).json({message: "Unauthorized"})
+    } catch (error) {
+        console.log({ error })
+        return res.status(401).json({ message: "Unauthorized" })
     }
 }
